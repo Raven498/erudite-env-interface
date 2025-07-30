567caa28d9d5e0b98528f2f595e29f8b7a084430
Nikilesh <78182016+Raven498@users.noreply.github.com>
2025-07-29 22:30:43 -0400

    add springboot test class

---
+++ src/test/java/erudite/env_interface/InterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package erudite.env_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
